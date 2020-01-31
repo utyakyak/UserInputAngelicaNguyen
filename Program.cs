18e0da80493798e28c1c530d347df26e20769620
Angelica Nguyen <Angelica@LAPTOP-C8KSJC6A>
2020-01-31 10:38:29 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UserInputAngelicaNguyen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, this is Angelica Nguyen");
+            Console.WriteLine("Say hi back and then hit enter!");
+            string greeting;
+            greeting = Console.ReadLine();
+            Console.WriteLine("You typed:" + greeting);
+
+        }
+    }
+}
